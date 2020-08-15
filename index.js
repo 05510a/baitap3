bc2b45cd6929d9de9f03e0087193b62c3f501d7f
Thang <0551a5@gmail.com>
2020-08-15 09:56:20 +0700

    buoi3

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-console.log('Làm bài tập');
-var btap1 = function(){
-    console.log('bài tập buổi 1');
-}
-var btap2 = function(){
-    console.log(done bài tập buổi 2)
-}
-var btap3 = function(){
-    console.log( done bài tập buổi 3)
-}
+
+console.log('buổi 3');
